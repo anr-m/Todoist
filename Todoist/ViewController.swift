9d6e4023391deff456741abfdad1668346a78d85
Anuarbek Mukhanov <anuarbek.mukhanov@gmail.com>
2019-10-23 15:52:12 +0500

    Initial Commit

---
+++ Todoist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoist
+//
+//  Created by Anuar on 10/23/19.
+//  Copyright © 2019 Anuarbek Mukhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
